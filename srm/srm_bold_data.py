b301ab951254d06d1bef2c174243c0c3083cd6be
oliver-contier <o.contier@gmail.com>
2019-10-10 15:10:32 +0200

    did the same for robust shared response model

--- srm/srm_bold_data.py
+++ srm/srm_bold_data.py
@@ -9,20 +9,20 @@ TODO: Generate and apply brain masks with BET.
 """
 
 import glob
-import numpy as np
 import os
 import pickle
 from os.path import join as pjoin
-from scipy import stats
 
+import numpy as np
 from brainiak.funcalign.srm import SRM
+from brainiak.funcalign.rsrm import RSRM
 from brainiak.io import load_images
+from scipy import stats
 
 
 def grab_bold_data(cond_id='D1_D5',
                    ds_dir='/data/BnB_USER/oliver/somato',
-                   testsubs=3,
-                   melodic_outdir=''):
+                   testsubs=False):
     """
     Get file paths for our bold data for a given run.
     """
@@ -31,19 +31,19 @@ def grab_bold_data(cond_id='D1_D5',
                for subdir in glob.glob(ds_dir + '/*')]
     if testsubs:
         sub_ids = sub_ids[:testsubs]
-    bold_files = [pjoin(ds_dir, sub_id, cond_id, 'data.nii.gz')
-                  for sub_id in sub_ids]
-    return bold_files, sub_ids
+    boldfiles = [pjoin(ds_dir, sub_id, cond_id, 'data.nii.gz')
+                 for sub_id in sub_ids]
+    return boldfiles, sub_ids
 
 
-def boldfiles_to_arrays(bold_files,
+def boldfiles_to_arrays(boldfiles,
                         z_score=True):
     """
     Load bold data into list of arrays (bc that's what brainiak wants)
     """
-    print('loading bold_files')
+    print('loading boldfiles')
     # create generator that returns nibabel nifti instances
-    nibs_gen = load_images(bold_files)
+    nibs_gen = load_images(boldfiles)
     # get numpy array data from each those instances (might take a while)
     print('converting to np arrays')
     bold_arrays_list = [np.reshape(nib_instance.get_fdata(), (1327104, 256))  # TODO: don't hard code this
@@ -62,12 +62,18 @@ def boldfiles_to_arrays(bold_files,
 
 
 def train_srm(training_data,
+              use_robust_srm=True,
               pickle_outdir='/home/homeGlobal/oli/somato/scratch/srm',
               n_comps=50,
               n_iters=20):
     # set up
     print('setting up srm')
-    srm = SRM(n_iter=n_iters, features=n_comps)
+    if use_robust_srm:
+        srm = RSRM(n_iter=n_iters, features=n_comps)
+        outpickle = pjoin(pickle_outdir, 'rsrm.p')
+    else:
+        srm = SRM(n_iter=n_iters, features=n_comps)
+        outpickle = pjoin(pickle_outdir, 'srm.p')
     # fit
     print('fitting srm')
     srm.fit(training_data)
@@ -76,14 +82,13 @@ def train_srm(training_data,
     if pickle_outdir:
         if not os.path.exists(pickle_outdir):
             os.makedirs(pickle_outdir)
-        outpickle = pjoin(pickle_outdir, 'srm.p')
         with open(outpickle, 'wb') as f:
             pickle.dump(srm, f)
     return srm
 
 
 if __name__ == '__main__':
-    bold_files, sub_ids = grab_bold_data()  # testsubs=3)
+    bold_files, subids = grab_bold_data()  # testsubs=3)
     bold_arrays = boldfiles_to_arrays(bold_files)
-    srm = train_srm(bold_arrays)  # , n_comps=5, n_iters=5)
+    srm_ = train_srm(bold_arrays)  # , n_comps=5, n_iters=5)
     print('done')
