00c6bf96f9e5d8a7389b6077ae80b71037816023
oliver-contier <o.contier@gmail.com>
2019-10-24 10:21:43 +0200

    roi_glm workflow runs cleanly. However, sorting out which contrast is which is very difficult. Maybe I should use modelfit as intended, which is to only take the runs of one subject (instead of running all subjects through this workflow)

--- roi_glm/roi_glm.py
+++ roi_glm/roi_glm.py
@@ -31,7 +31,8 @@ def grab_bold_data(cond_id='D1_D5',
 
 
 def make_bunch_and_contrasts(n_cycles=20,
-                             dur_per_digit=5.12):
+                             dur_per_digit=5.12,
+                             n_subs=12):
     from nipype.interfaces.base import Bunch
     cycle_dur = dur_per_digit * 5
     conditions = ['D_%i' % i for i in range(1, 6)]
@@ -39,17 +40,27 @@ def make_bunch_and_contrasts(n_cycles=20,
                for cycle_idx in range(n_cycles)]
               for digit_idx in range(5)]
     durations = [[dur_per_digit] * n_cycles for _ in range(5)]
-    subject_info = Bunch(conditions=conditions, onsets=onsets, durations=durations)
+    subject_info = [Bunch(conditions=conditions, onsets=onsets, durations=durations)
+                    for _ in range(n_subs)]
     # t-cotrasts
     t_contrasts = []
     for cond in conditions:
         contrast_vector = [0, 0, 0, 0]
         contrast_vector.insert(conditions.index(cond), 1)
-        t_contrasts.append(['tcon_%s' % cond, 'T', conditions, contrast_vector])
+        t_contrasts.append(('tcon_%s' % cond, 'T', conditions, contrast_vector))
     # f-contrast over all t-contrasts
-    f_contrast = [['All_Digits', 'F', t_contrasts]]
+    f_contrast = [('All_Digits', 'F', t_contrasts)]
     contrasts = t_contrasts + f_contrast
     return subject_info, contrasts
+
+
+def flatten_nested_list(nested_list):
+    """
+    Seems like a bit of a design flaw that this is even necessary,
+    but oh, well ...
+    """
+    flat_list = [item for sublist in nested_list for item in sublist]
+    return flat_list
 
 
 def create_main_wf(wf_workdir='/data/BnB_USER/oliver/somato/scratch/roi_glm',
@@ -101,12 +112,18 @@ def create_main_wf(wf_workdir='/data/BnB_USER/oliver/somato/scratch/roi_glm',
                      name='designgen')
 
     # creates (intermediate) fsl compatible design file 'session_info' for modelfit
-    modelspec = MapNode(SpecifyModel(input_units='secs'), name='modelspec', iterfield=['functional_runs'])
+    modelspec = MapNode(SpecifyModel(input_units='secs'), name='modelspec',
+                        iterfield=['functional_runs', 'subject_info'])
     modelspec.inputs.input_units = 'secs'
     modelspec.inputs.time_repetition = tr
     modelspec.inputs.high_pass_filter_cutoff = hp_vols * tr
 
+    flatten_session_infos = Node(Function(input_names=['nested_list'], output_names=['flat_list'],
+                                          function=flatten_nested_list), name='flatten_session_infos')
+
     # Modelfit is an extra built-in workflow
+    # TODO: this modelfit workflow is originally meant to estimate all runs from one subject.
+    #  Since our subjects all have same onsets, this works across subjects as well. But keep in mind for later
     modelfit = create_modelfit_workflow(f_contrasts=True)
     modelfit.inputs.inputspec.interscan_interval = tr
     modelfit.inputs.inputspec.film_threshold = film_thresh
@@ -119,7 +136,9 @@ def create_main_wf(wf_workdir='/data/BnB_USER/oliver/somato/scratch/roi_glm',
     wf.connect(susan, 'smoothed_file', bpf, 'in_file')
     wf.connect(bpf, 'out_file', modelspec, 'functional_runs')
     wf.connect(designgen, 'subject_info', modelspec, 'subject_info')
-    wf.connect(modelspec, 'session_info', modelfit, 'inputspec.session_info')
+    wf.connect(modelspec, 'session_info', flatten_session_infos, 'nested_list')
+    wf.connect(flatten_session_infos, 'flat_list', modelfit, 'inputspec.session_info')
+    # wf.connect(modelspec, 'session_info', modelfit, 'inputspec.session_info')
     wf.connect(designgen, 'contrasts', modelfit, 'inputspec.contrasts')
     wf.connect(bpf, 'out_file', modelfit, 'inputspec.functional_data')
 
