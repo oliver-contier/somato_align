72a34ada0227a69562966344a6b15f66b5956db4
oliver-contier <o.contier@gmail.com>
2019-11-20 23:09:03 +0100

    crossvalidated projection works. started plotting results

--- srm/crossval_projection.py
+++ srm/crossval_projection.py
@@ -32,36 +32,25 @@ def project_data_crossval(run1_arrs,
         # select run used for training and test
         training_arrs = (run1_arrs, run2_arrs)[trainrun_idx]
         test_arrs = (run1_arrs, run2_arrs)[abs(trainrun_idx - 1)]
-        # prepare results array for all subjects (shape nsubs, k, nvols) projected data
-        # and our shared responses from the training data
+        # prepare results arrays (for projected data and estimated srm time series)
         results_array = np.zeros(shape=(len(training_arrs), k, training_arrs[0].shape[1]))
         trained_srms_array = np.zeros(shape=(len(run1_arrs), k, training_arrs[0].shape[1]))
         # iterate over testsubjects
         for testsub_idx in range(len(training_arrs)):
             start = time.time()
             print('starting run %i subject %i' % (trainrun_idx, testsub_idx))
-            # select training data
-            trainsubs_traindata = [x for i, x in enumerate(training_arrs) if i != testsub_idx]
+            trainsubs_traindata = [x for i, x in enumerate(training_arrs) if i != testsub_idx]  # select training data
             testsub_traindata = training_arrs[testsub_idx]
-            # train srm on training subject's training data
-            srm = RSRM(n_iter=niter, features=k)
-            print('fitting srm')
+            srm = RSRM(n_iter=niter, features=k)  # train srm on training subject's training data
             srm.fit(trainsubs_traindata)
-            # estimate test subject's bases
-            print('estimating subjects bases')
-            w, s = srm.transform_subject(testsub_traindata)
+            w, s = srm.transform_subject(testsub_traindata)  # estimate test subject's bases
             # reattach weight matrix and individual term to srm instance
             # (to allow transforming test run with builtin brainiak function)
             srm.w_.insert(testsub_idx, w)
             srm.s_.insert(testsub_idx, s)
-            # project test run into shared space
-            print('projecting data')
-            projected_data, ind_terms = srm.transform(test_arrs)
-            # select projected data from test subject
-            testsub_proj = projected_data[testsub_idx]
-            # append result to our results arrays
-            print('saving results')
-            results_array[testsub_idx] = testsub_proj
+            projected_data, ind_terms = srm.transform(test_arrs)  # project test run into shared space
+            testsub_proj = projected_data[testsub_idx]  # select projected data from test subject
+            results_array[testsub_idx] = testsub_proj  # append result to our results arrays
             trained_srms_array[testsub_idx] = srm.r_
             elapsed = time.time() - start
             print('this round took: ', elapsed)
@@ -71,9 +60,9 @@ def project_data_crossval(run1_arrs,
             np.save(outf, results_array)
         trained_srms_outpath = pjoin(outdir, 'trainedsrms_run%i_is_train.npy' % (trainrun_idx + 1))
         with open(trained_srms_outpath, 'wb') as outf:
-            np.save(outf, results_array)
-
-        return None
+            np.save(outf, trained_srms_array)
+    print('done!')
+    return None
 
 
 if __name__ == '__main__':
